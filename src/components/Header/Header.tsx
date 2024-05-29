0d012222dbfbd103ce31b0e56814423f7c19bab6
Code_Junior <essienmichael4@gmail.com>
2024-05-29 12:09:33 +0000

    Hero section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,11 +11,11 @@ const Header = () => {
 }
 
 function Desktop(){
-    return <header className='hidden fixed w-full h-[6rem] bg-[#0c0c0c57] backdrop-blur-sm md:block'>
-    <div className="container mx-auto flex h-full justify-between items-center">
+    return <header className='hidden fixed top-0 w-full h-[6rem] bg-[#0c0c0c57] backdrop-blur-sm md:block z-20'>
+    <div className="container px-8 mx-auto flex h-full justify-between items-center">
         <Link to="/"><h1 className='island text-6xl text-white'>ME</h1></Link>
         <nav className='flex h-full items-center gap-8'>
-            <ul className='flex gap-4 h-full items-center'>
+            <ul className='flex gap-8 h-full items-center'>
                 <li className='list-none h-full'><Link to="/" className='flex items-center justify-center px-2 text-white h-full  border-b-4 border-white'>Home</Link></li>
                 <li className='list-none h-full'><Link to="/" className='flex items-center justify-center px-2 text-white h-full'>Projects</Link></li>
                 <li className='list-none h-full'><Link to="/" className='flex items-center justify-center px-2 text-white h-full'>About</Link></li>
@@ -31,7 +31,7 @@ function Mobile(){
     const [isOpen, setIsOpen]=useState(false)
     return (
         <>
-            <header className='block bg-[#0c0c0c57] h-[4rem] backdrop-blur-sm md:hidden'>
+            <header className='block fixed top-0 w-full bg-[#0c0c0c57] h-[4rem] backdrop-blur-sm md:hidden z-20'>
                 <div className="container mx-auto h-full flex justify-between items-center px-4">
                     <Link to="/"><h1 className='island text-4xl text-white'>ME</h1></Link>
 
@@ -40,7 +40,7 @@ function Mobile(){
                     }}></div>
                 </div>
             </header>
-            <nav className={`${isOpen ? "fixed inset-0 flex": "hidden"} bg-[#0c0c0c57] backdrop-blur-sm items-center justify-center gap-8`}>
+            <nav className={`${isOpen ? "fixed inset-0 flex": "hidden"} bg-[#0c0c0c57] backdrop-blur-sm items-center justify-center gap-8 z-50`}>
             <div className='absolute right-4 top-4 w-8 h-8 bg-black' onClick={()=>{
                         setIsOpen(prev => !prev)
                     }}></div>
