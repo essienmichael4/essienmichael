2b4cd33c8ec8c3be3f58b7039872d9bfbfbca26f
Code_Junior <essienmichael4@gmail.com>
2024-05-29 16:39:47 +0000

    Projects section

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
 import Hero from "../../components/Hero/Hero"
+import Project from "../../components/Project/Project"
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         <Header />
         <main className="w-full">
             <Hero />
+            <Project />
         </main>
         <hr />
         <Footer />
